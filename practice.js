f60efba6727b91a7ab03426b30fee0c3ab28cc6b
Jordan Daniels <decentpianist@gmail.com>
2017-11-06 20:37:28 -0800

    solved reverse integer

---
+++ practice.js
@@ -0,0 +1,16 @@
+//11/6/2017
+// reverse a number 
+var reverse = function(x) {
+    const reverseArray = [];
+    let next;
+    while(x){
+    		let digit = x % 10
+        reverseArray.push(digit)
+        x = Math.floor(Math.abs((x/10)))
+        console.log(x, 'digit')
+    }
+    return reverseArray.join('')
+    
+}
+
+console.log(reverse(-30))
