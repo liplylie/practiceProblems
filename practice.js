6fcbd8c64795ee8442014c006b9700f2d92abdc3
Jordan Daniels <decentpianist@gmail.com>
2017-11-06 20:48:47 -0800

    solved merge linked list

--- practice.js
+++ practice.js
@@ -13,4 +13,17 @@ var reverse = function(x) {
     
 }
 
-console.log(reverse(-30))
+// Merge two sorted linked lists and return it as a new list. 
+//The new list should be made by splicing together the nodes of the first two lists.
+let link1 = {value:1, next: null}
+let link2 = {value:2, next: null}
+var mergeTwoLists = function(l1, l2) {
+	let temp = l1;
+	while(temp.next){
+		temp = temp.next
+	}
+	temp.next = l2
+	return l1
+}
+
+//console.log(mergeTwoLists(link1,link2))
