04ce26a240975356392bf609b4f38d4929e624c1
Jordan Daniels <decentpianist@gmail.com>
2017-11-29 16:05:02 -0800

    refactor python code

--- practice.py
+++ practice.py
@@ -7,5 +7,15 @@ class Solution(object):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
-x = Solution()
-print(x.twoSum([3,2,4], 6))
+    def twoSumRefactor(self, nums, target):
+        if len(nums) < 1:
+            return false
+        hold = {}
+        for i in range(len(nums)):
+            if nums[i] in hold:
+            	return [hold[nums[i]], i]
+            else:
+            	hold[target - nums[i]] = i
+# x = Solution()
+# print(x.twoSum([3,2,4], 6))
+# print(x.twoSumRefactor([3,2,4], 6))
