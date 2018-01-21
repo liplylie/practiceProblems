86f900271d3a66e98af9e9e2594eaf7f373d0726
Jordan Daniels <decentpianist@gmail.com>
2018-01-20 23:56:53 -0800

    finished js problem

--- practice.js
+++ practice.js
@@ -1,3 +1,25 @@
+// 1/20/2018
+
+var diameterOfBinaryTree = function(root) {
+    var maxDiameter = -1;
+    var getDepth = function(root) {
+        if (root === null) {
+            return 0;
+        }
+        var leftDepth = getDepth(root.left);
+        var rightDepth = getDepth(root.right);
+        
+        maxDiameter = Math.max(maxDiameter, leftDepth + rightDepth);
+        maxDepth = Math.max(leftDepth, rightDepth) + 1;
+        return maxDepth;
+    };
+    
+    if (root === null) return 0;
+    getDepth(root);
+    
+    return maxDiameter;
+};
+
 // 1/18/2018
 
 var convertBST = function(root) {
