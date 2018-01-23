eb36ecced0f1492e5d2ffe27f7a1f9ce3efddc1d
Jordan Daniels <decentpianist@gmail.com>
2018-01-23 13:46:48 -0800

    finished js problem

--- practice.js
+++ practice.js
@@ -1,3 +1,21 @@
+// 1/23/2018
+
+var fizzBuzz = function(n) {
+    const results = []
+    for ( let i = 1; i <= n; i++){
+        if (i % 3 === 0 && i % 5 === 0) {
+        results.push("FizzBuzz")
+        } else if (i % 3 === 0){
+            results.push("Fizz")
+        } else if (i % 5 === 0){
+            results.push("Buzz")
+        } else {
+        results.push(i.toString())
+        }
+    }
+    return results
+};
+
 // 1/20/2018
 
 var diameterOfBinaryTree = function(root) {
