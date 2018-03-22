8d7f7fb31e844dd7addc2b3ff8f1683fa0725663
Jordan Daniels <decentpianist@gmail.com>
2018-03-22 16:25:53 -0700

    added selection sort

--- practice.js
+++ practice.js
@@ -1,5 +1,18 @@
 // 3/22/2018
-
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++){
+    let smallest = i
+    for (let j = i+1; j < arr.length; j++){
+      if (arr[j] < arr[smallest]){
+        smallest = j
+      }
+    }
+    let temp = arr[i]
+      arr[i] = arr[smallest]
+      arr[smallest] = temp
+  }
+  return arr
+}
 
 
 let bubbleSort = function(array) {
