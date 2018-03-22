9e0bda9ed0548deee6c343f5182b53fa434cc640
Jordan Daniels <decentpianist@gmail.com>
2018-03-22 15:29:03 -0700

    finished bubble sort

--- practice.js
+++ practice.js
@@ -1,3 +1,20 @@
+// 3/22/2018
+
+
+let bubbleSort = function(array) {
+    for (let i = 0; i < array.length; i++){
+      for (let j = i+1; j < array.length; j++){
+        if (array[i] > array[j]){
+          let temp = array[i]
+          array[i] = array[j]
+          array[j] = temp
+        }
+      }
+    }
+    return array
+};
+
+
 //3/20/2018
 
 function spiralTraversal (matrix) {
